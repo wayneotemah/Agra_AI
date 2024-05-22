3c232dac043c46e4eaedd2fbfb0934aac088e6be
Wayne <wayneotemahegesa@gmail.com>
2024-05-22 12:35:47 +0300

    Small changes

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -3,19 +3,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('djano_secret_key')
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
