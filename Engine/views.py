dc9dac96192b2bd1fa659b123cb42f45e32ddc78
wayne <wayneotemahegesa@gmail.com>
2024-05-26 23:44:26 +0000

    fixed: reponce with voice output

--- Engine/views.py
+++ Engine/views.py
@@ -33,18 +33,18 @@ class AudioQueryView(APIView):
     """
     def post(self, request):
         try:
-            # generate response using llm_answer
             query = request.data.get("query")
             if query is None:
                 return Response({"error": "No query provided"}, status=status.HTTP_400_BAD_REQUEST)
 
+            # generate response using llm_answer
             response = llm_answer(query)
-            # convert response to speech
 
+            
             filename = f"{uuid.uuid4()}.mp3"
-            if tts(response,filename):
+            if tts(response,filename): # convert response to speech
                 audio_file_url = os.path.join(settings.MEDIA_URL, "audio", filename)
-                return Response({'query': query, 'audio_file_url': audio_file_url}, status=status.HTTP_200_OK)
+                return Response({'response': response, 'audio_file_url': audio_file_url}, status=status.HTTP_200_OK)
             else:
                 return Response({"error": "Error in converting text to speech"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
